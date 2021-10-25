f40aefbbc2970529b918fd77645aab6e58ce5c66
Anthony Bosch <boscha1@mymail.nku.edu>
2021-10-25 14:44:20 -0400

    Added try catch for image picking

--- app/src/main/java/com/csc415/photoeditor/PhotoEditorActivity.kt
+++ app/src/main/java/com/csc415/photoeditor/PhotoEditorActivity.kt
@@ -10,17 +10,20 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.csc415.photoeditor.transform.ColorBalance
 import com.csc415.photoeditor.transform.Exposure
 import java.io.File
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 
 
 class PhotoEditorActivity : AppCompatActivity()
 {
 	private val tag = PhotoEditorActivity::class.java.simpleName
 	private lateinit var imageUri : String
+	private lateinit var bitmap: Bitmap
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -34,24 +37,32 @@ class PhotoEditorActivity : AppCompatActivity()
 			imageUri = intent.getStringExtra(IntentExtraMessage.PHOTO_URI.extraName)!!
 			Log.d(tag, imageUri)
 
-			var bitmap = BitmapFactory.decodeStream(FileInputStream(File(imageUri)))
-			val exif = ExifInterface(imageUri)
-			val orientation: Int = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1)
-			val matrix = Matrix()
+			try {
+				bitmap = BitmapFactory.decodeStream(FileInputStream(File(imageUri)))
+				val exif = ExifInterface(imageUri)
+				val orientation: Int = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1)
+				val matrix = Matrix()
 
-			Log.d("EXIF", "Exif: $orientation")
+				Log.d("EXIF", "Exif: $orientation")
 
-			// Make sure that the image rotation is correct. Sometimes it likes to be off +/- 90 degrees.
-			when (orientation)
-			{
-				6 -> matrix.postRotate(90F)
-				3 -> matrix.postRotate(180F)
-				8 -> matrix.postRotate(270F)
+				// Make sure that the image rotation is correct. Sometimes it likes to be off +/- 90 degrees.
+				when (orientation)
+				{
+					6 -> matrix.postRotate(90F)
+					3 -> matrix.postRotate(180F)
+					8 -> matrix.postRotate(270F)
+				}
+
+				// Recreate the bitmap using the rotation matrix.
+				bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+				findViewById<ImageView>(R.id.photo).setImageBitmap(bitmap)
+			} catch (e: FileNotFoundException) {
+				Log.e(tag, "Image file not found. Falling back to MainActivity", e)
+				startActivity(Intent(this, MainActivity::class.java))
+				val toast = Toast.makeText(applicationContext, "File Not Found", Toast.LENGTH_LONG)
+				toast.show()
 			}
 
-			// Recreate the bitmap using the rotation matrix.
-			bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-			findViewById<ImageView>(R.id.photo).setImageBitmap(bitmap)
 		}
 		else // Invalid intent.
 		{
