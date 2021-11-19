612731263c96b304ec84033d001a86b8cf524dfa
boscha1 <bosch.anthony7@gmail.com>
2021-11-18 21:18:53 -0500

    First commit

---
+++ app/src/main/java/com/csc415/photoeditor/model/BitmapViewModel.kt
@@ -0,0 +1,15 @@
+package com.csc415.photoeditor.model
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+
+/**
+ * View model so we can save the bitmap during a configuration change (ie orientation change).
+ *
+ * @author Will St. Onge
+ */
+class BitmapViewModel : ViewModel()
+{
+    var bitmap: Bitmap? = null
+    var originalImage: Bitmap? = null
+}
